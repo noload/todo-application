daa411007158d5bf5c7479f6c7b2fbcbb73ff5b9
noload <vaibhavkamble023@gmail.com>
2024-04-01 20:41:50 +0530

    todo application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React from "react";
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
+import { Provider } from "react-redux";
+import store from "./components/store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Simple To-Do Application</h1>
+        <TaskInput></TaskInput>
+        <TaskList></TaskList>
+      </div>
+    </Provider>
   );
 }
 
